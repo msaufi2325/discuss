8d4e972741ccbc77d39a2a0995baf43d0407851f
msaufi2325 <msaufi2325@gmail.com>
2024-07-13 15:35:34 +0900

    Add NextUI components to TopicCreateForm

--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -1,3 +1,12 @@
+import {
+  Input,
+  Button,
+  Textarea,
+  Popover,
+  PopoverTrigger,
+  PopoverContent
+} from '@nextui-org/react';
+
 export default function TopicCreateForm () {
   return <div>Topic Create Form</div>
 }
