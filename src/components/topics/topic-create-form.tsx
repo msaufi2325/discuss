f82a766908ce034f4837d6cba088f7bd09e627ec
msaufi2325 <msaufi2325@gmail.com>
2024-07-14 18:02:39 +0900

    Update TopicCreateForm component with NextUI components

--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -29,7 +29,7 @@ export default function TopicCreateForm() {
               labelPlacement="outside"
               placeholder="Name"
               isInvalid={!!formState.errors.name}
-              errorMessage={formState.errors.name?.join(', ')}
+              errorMessage={formState.errors.name?.join(", ")}
             />
             <Textarea
               name="description"
@@ -37,10 +37,14 @@ export default function TopicCreateForm() {
               labelPlacement="outside"
               placeholder="Describe your topic"
               isInvalid={!!formState.errors.description}
-              errorMessage={formState.errors.description?.join(', ')}
+              errorMessage={formState.errors.description?.join(", ")}
             />
 
-            {formState.errors._form?.join(', ')}
+            {formState.errors._form ? (
+              <div className="rounded p-2 bg-red-200 border border-red-400">
+                {formState.errors._form?.join(", ")}
+              </div>
+            ) : null}
 
             <Button type="submit">Submit</Button>
           </div>
