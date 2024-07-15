7c7bde2444599571f4eb8c6bf7650954a9c0134e
msaufi2325 <msaufi2325@gmail.com>
2024-07-15 21:51:09 +0900

    Remove delay in createTopic function for form submission

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -22,8 +22,6 @@ interface CreateTopicFormState {
 }
 
 export async function createTopic(formState: CreateTopicFormState, formData: FormData): Promise<CreateTopicFormState> {
-  await new Promise(resolve => setTimeout(resolve, 2500));
-
   const result = createTopicSchema.safeParse({
     name: formData.get('name'),
     description: formData.get('description'),
