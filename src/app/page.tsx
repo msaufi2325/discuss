298c3ad2d83b6ede6e7146b2b6b03399ca229ce7
msaufi2325 <msaufi2325@gmail.com>
2024-07-15 22:21:24 +0900

    Add TopicList component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import TopicCreateForm from "@/components/topics/topic-create-form"
+import TopicList from "@/components/topics/topic-list"
 
 export default function Home() {
   return <div className="grid grid-cols-4 gap-4 p-4">
@@ -7,6 +8,7 @@ export default function Home() {
     </div>
     <div>
       <TopicCreateForm />
+      <TopicList />
     </div>
   </div>
 }
