540a8b7172148403b6e5b5e96785744fd6547900
msaufi2325 <msaufi2325@gmail.com>
2024-07-17 03:04:30 +0900

    createPost function to handle authentication and form validation

--- src/actions/create-post.ts
+++ src/actions/create-post.ts
@@ -1,15 +1,15 @@
-'use server';
+"use server";
 
-import type { Post } from '@prisma/client';
-import { revalidatePath } from 'next/cache';
-import { z } from 'zod';
-import { auth } from '@/auth';
-import { db } from '@/db';
-import paths from '@/path';
+import type { Post } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+import { z } from "zod";
+import { auth } from "@/auth";
+import { db } from "@/db";
+import paths from "@/path";
 
 const createPostSchema = z.object({
   title: z.string().min(3),
-  content: z.string().min(10)
+  content: z.string().min(10),
 });
 
 interface CreatePostFormState {
@@ -17,33 +17,48 @@ interface CreatePostFormState {
     title?: string[];
     content?: string[];
     _form?: string[];
-  }
+  };
 }
 
-export async function createPost(formState: CreatePostFormState, formData: FormData): Promise<CreatePostFormState> {
+export async function createPost(
+  slug: string,
+  formState: CreatePostFormState,
+  formData: FormData
+): Promise<CreatePostFormState> {
   const result = createPostSchema.safeParse({
-    title: formData.get('title'),
-    content: formData.get('content')
-  })
+    title: formData.get("title"),
+    content: formData.get("content"),
+  });
 
   if (!result.success) {
     return {
-      errors: result.error.flatten().fieldErrors
-    }
+      errors: result.error.flatten().fieldErrors,
+    };
   }
 
   const session = await auth();
   if (!session || !session.user) {
     return {
       errors: {
-        _form: ['You must be signed in to create a post'],
+        _form: ["You must be signed in to create a post"],
+      },
+    };
+  }
+
+  const topic = await db.topic.findFirst({
+    where: { slug }
+  });
+
+  if (!topic) {
+    return {
+      errors: {
+        _form: ["Topic not found"],
       },
     };
   }
 
   return {
-    errors: {}
-  }
+    errors: {},
+  };
   //TODO: revalidate the topic show page
-
 }
