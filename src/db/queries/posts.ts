73a6028abd0728d3528de2852b1d8e587d7b8f44
msaufi2325 <msaufi2325@gmail.com>
2024-07-20 15:01:12 +0900

    Refactor PostList component to use async fetchData function

--- src/db/queries/posts.ts
+++ src/db/queries/posts.ts
@@ -1,7 +1,7 @@
 import type { Post } from '@prisma/client';
 import { db } from '@/db';
 
-type PostWithData = (
+export type PostWithData = (
   Post & {
     topic: { slug: string };
     user: { name: string | null };
