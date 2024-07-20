db0b9d1167d74011fa50267c62417f4cbaa457b0
msaufi2325 <msaufi2325@gmail.com>
2024-07-20 09:35:38 +0900

    Update fetchPostsByTopicSlug

--- src/db/queries/posts.ts
+++ src/db/queries/posts.ts
@@ -4,6 +4,18 @@ import { db } from '@/db';
 type PostWithData = (
   Post & {
     topic: { slug: string };
-    user: { name: string };
+    user: { name: string | null };
+    _count: { comments: number };
   }
 );
+
+export function fetchPostsByTopicSlug(slug: string): Promise<PostWithData[]> {
+  return db.post.findMany({
+    where: { topic: { slug }},
+    include: {
+      topic: { select: { slug: true }},
+      user: { select: { name: true }},
+      _count: { select: { comments: true }}
+    }
+  })
+};
